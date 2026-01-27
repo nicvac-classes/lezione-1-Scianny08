d388e0eb84f6ecc6ef1278a7e289df4da8dc0f93
Scianny08 <giubanni17@gmail.com>
2026-01-27 17:28:27 +0100

    pygame extra

--- pygame/Lezione01.py
+++ pygame/Lezione01.py
@@ -1,3 +1,4 @@
+import random
 import pygame
 import sys
 
@@ -6,8 +7,10 @@ screen = pygame.display.set_mode((640, 480))
 pygame.display.set_caption("Test Pygame")
 
 x, y = 320, 240
+raggio = 30
 speed = 5
 clock = pygame.time.Clock()
+colore = (255, 0, 0)
 
 running = True
 while running:
@@ -18,7 +21,7 @@ while running:
             sys.exit()
             
     keys = pygame.key.get_pressed()
-    
+
     if keys[pygame.K_LEFT]:
         x -= speed
     
@@ -30,15 +33,33 @@ while running:
         
     if keys[pygame.K_UP]:
         y -= speed
-        
+
+    if keys[pygame.K_SPACE]:
+        colore = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
     
+    if keys[pygame.K_PLUS] or pygame.K_KP_PLUS:
+        raggio += 1
+        
+    #sinistra
+    if x < raggio: x = raggio
+
+    #destra
+    if x > 640 - raggio: x = 640 - raggio
+
+    #sotto
+    if y < raggio: y = raggio
+
+    #sopra
+    if y > 480 - raggio: y = 480 - raggio
+
+
     screen.fill((30, 30, 50))
     
     cerchio = pygame.draw.circle(
         screen,         #surface
-        (255, 0, 0),    #colore rgb
+        colore,         #colore rgb
         (x, y),         #x, y
-        30              #raggio
+        raggio              #raggio
     )
     
     pygame.display.flip()
